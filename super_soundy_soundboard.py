63436a7e1d1ac2d127b0cc37583005aa84d01f89
Nikita <idexstandoff@gmail.com>
2024-07-06 15:38:22 +0300

    made a working screen with no functionality

--- super_soundy_soundboard.py
+++ super_soundy_soundboard.py
@@ -1,17 +1,35 @@
 import pygame
 import sys
-import random
 
 #colors
 black = [0, 0, 0]
 white = [255, 255, 255]
 gray = [150,150,150]
+blue = [0,0,240]
 
 
-pygame.init
 screen_size=[840,480]
+pygame.init()
 screen=pygame.display.set_mode(screen_size)
 background_color=black
 pygame.display.set_caption('super soundy soundboard')
 
+
 screen_works = True
+while screen_works:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            screen_works = False
+
+    screen.fill(black)
+
+    #commands for keys
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_q]:
+        screen_works = False
+        pygame.quit()
+        sys.exit()
+
+    pygame.display.flip()
+
