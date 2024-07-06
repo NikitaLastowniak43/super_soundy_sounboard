937842d407914761cb39d4538a4e31738facfa2c
Nikita <idexstandoff@gmail.com>
2024-07-06 14:59:16 +0300

    imported 3 libraries and some screen things

---
+++ super_soundy_soundboard.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+import random
+
+#colors
+black = [0, 0, 0]
+white = [255, 255, 255]
+gray = [150,150,150]
+
+
+pygame.init
+screen_size=[840,480]
+screen=pygame.display.set_mode(screen_size)
+background_color=black
+pygame.display.set_caption('super soundy soundboard')
+
+screen_works = True
