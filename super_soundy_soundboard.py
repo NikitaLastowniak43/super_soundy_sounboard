804e6286ccdab3ee002da8da26a98dd722750568
Nikita <idexstandoff@gmail.com>
2024-07-08 23:15:26 +0300

    made background a bit better and added start button

--- super_soundy_soundboard.py
+++ super_soundy_soundboard.py
@@ -7,7 +7,7 @@ import time
 black = [0, 0, 0]
 white = [255, 255, 255]
 gray = [150,150,150]
-blue = [0,0,240]
+back_ground = [60,100,160]
 
 
 screen_size=[840,480]
@@ -26,7 +26,7 @@ def Menu():
     super_small_font = pygame.font.SysFont(None, 16)
     small_font = pygame.font.SysFont(None, 36)
     mid_font = pygame.font.SysFont(None, 52)
-    large_font = pygame.font.SysFont(None, 40)
+    large_font = pygame.font.SysFont(None, 80)
 
     screen_works = True
     while screen_works:
@@ -35,23 +35,39 @@ def Menu():
             if event.type == pygame.QUIT:
                 screen_works = False
 
-        screen.fill(black)
+        screen.fill(back_ground)
 
-        #texts and buttons
+        #texts, buttons and figures
         mouse_cord = pygame.mouse.get_pos()
-        draw_text("Super Soundy Soundboard", mid_font, white, screen, 30, 50)
-        draw_text("totaly NOT a button", super_small_font,white,screen,490, 68)
 
+        frame_for_name = pygame.Rect(10, 40, 600, 50)
+        pygame.draw.rect(screen, white, frame_for_name, 2)
+        draw_text("Super Soundy Soundboard", mid_font, white, screen, 30, 50)
+        draw_text("totaly NOT a button", super_small_font, white, screen, 490, 68)
+
+
+        frame_for_start_button = pygame.Rect(10, 155, 170, 60)
+        pygame.draw.rect(screen, white, frame_for_start_button, 2)
+        draw_text("Start", large_font, white, screen, 30, 160)
+
+        #interaction with menu NOT button
         if mouse_cord[0] > 50 and mouse_cord[0] < 500:
             if mouse_cord[1] > 50 and mouse_cord[1] < 80:
                 if pygame.mouse.get_pressed()[0]:
                     draw_text("Super Soundy Soundboard", mid_font, gray, screen, 30, 50)
                     secret = pygame.mixer.Sound("fart-83471.mp3")
                     secret.play()
-                    # time.sleep(100)
-                    # secret.stop()
 
-        #commands for keys
+        #interaction with start button
+        if mouse_cord[0] > 10 and mouse_cord[0] < 180:
+            if mouse_cord[1] > 148 and mouse_cord[1] < 220:
+                pygame.draw.rect(screen, gray, frame_for_start_button, 2)
+                draw_text("Start", large_font, gray, screen, 30, 160)
+                if pygame.mouse.get_pressed()[0]:
+                    secret = pygame.mixer.Sound("fart-83471.mp3")
+                    secret.play()
+
+        #if keys are pressed
         keys = pygame.key.get_pressed()
         if keys[pygame.K_q]:
             screen_works = False
@@ -61,3 +77,4 @@ def Menu():
         pygame.display.flip()
 
 Menu()
+
